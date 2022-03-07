eb340a69ba1140df55480e5c8fab24b8a2b61e1b
DeekshithSN <deekshithsn@gmail.com>
2022-03-07 06:58:59 +0530

    adding monitoring to python app

--- wishlist-microservice-python/Dockerfile
+++ wishlist-microservice-python/Dockerfile
@@ -1,4 +1,4 @@
 FROM python:3
 COPY . .
-RUN pip install flask flask_cors
+RUN pip install -r requirements.txt
 CMD ["python","index.py"]
--- wishlist-microservice-python/index.py
+++ wishlist-microservice-python/index.py
@@ -1,14 +1,27 @@
 from flask import Flask,request
 from flask_cors import CORS 
 import json
-
+import prometheus_client
+from prometheus_client.core import CollectorRegistry
+from prometheus_client import Summary, Counter, Histogram, Gauge
+import time
 
 app = Flask(__name__)
 CORS(app)
 
+_INF = float("inf")
+
+graphs = {}
+graphs['c'] = Counter('python_request_operations_total', 'The total number of processed requests')
+graphs['h'] = Histogram('python_request_duration_seconds', 'Histogram for the duration in seconds.', buckets=(1, 2, 5, 6, 10, _INF))
+
 
 @app.route('/', methods = ['GET'])
 def hello():
+   start = time.time()
+    graphs['c'].inc()
+    end = time.time()
+    graphs['h'].observe(end - start)
    print('Getting List of WishList Items')
    x = {
     "1": "Apple Iphone",
@@ -17,6 +30,15 @@ def hello():
    }
    y = json.dumps(x)
    return y
+
+
+@app.route("/metrics")
+def requests_count():
+    res = []
+    for k,v in graphs.items():
+        res.append(prometheus_client.generate_latest(v))
+    return Response(res, mimetype="text/plain")
+
 
 
 @app.route('/likes', methods = ['GET'])
